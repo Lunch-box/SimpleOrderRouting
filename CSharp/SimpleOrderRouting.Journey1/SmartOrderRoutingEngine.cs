f3dd58296f08410e96f2f86592fae53bb4ec1708
Thomas PIERRAIN <thomas@pierrain.net>
2014-09-08 13:01:42 +0200

    Refactor SorEngine.Route so that job in done in three steps.
    Add the concepts of OrderDescription and OrderBasket

--- CSharp/SimpleOrderRouting.Journey1/SmartOrderRoutingEngine.cs
+++ CSharp/SimpleOrderRouting.Journey1/SmartOrderRoutingEngine.cs
@@ -16,6 +16,7 @@
 namespace SimpleOrderRouting.Journey1
 {
     using System;
+    using System.Collections.Generic;
 
     public class SmartOrderRoutingEngine
     {
@@ -28,35 +29,32 @@ namespace SimpleOrderRouting.Journey1
 
         public void Route(InvestorInstruction investorInstruction)
         {
-            int remainingQuantityToBeExecuted = investorInstruction.Quantity;
-            
-            // Checks liquidities availabe to weighted average for execution
-            var availableQuantityOnMarkets = 0;
-            foreach (var market in this.markets)
+            // 1. Digest Investment instructions
+            // 2. Prepare order book (solver)
+            // 3. Send and monitor
+            // 4. Feedback investor
+
+            //2.
+            var orderBasket = this.SolveMarketSweep(investorInstruction);
+
+            //3.
+
+            ExecuteOrderBasket(orderBasket, investorInstruction);
+        }
+
+        private static void ExecuteOrderBasket(IEnumerable<OrderDescription> orderBasket, InvestorInstruction investorInstruction)
+        {
+            foreach (var orderDescription in orderBasket)
             {
-                if (investorInstruction.Price >= market.SellPrice)
-                {
-                    availableQuantityOnMarkets += market.SellQuantity;
-                }
-            }
-
-            decimal ratio = ((decimal)remainingQuantityToBeExecuted / (decimal)availableQuantityOnMarkets);
-            decimal roundedRatio = Math.Round(ratio, 2, MidpointRounding.AwayFromZero);
-
-            foreach (var market in this.markets)
-            {
-                decimal convertedMarketQuantity = Math.Round((market.SellQuantity * ratio), 2, MidpointRounding.AwayFromZero);
-                int quantityToExecute = Convert.ToInt32(convertedMarketQuantity);
-
-                var order = market.CreateLimitOrder(investorInstruction.Way, investorInstruction.Price, quantityToExecute, true);
-                
+                var market = orderDescription.TargetMarket;
+                var order = market.CreateLimitOrder(orderDescription.OrderWay, orderDescription.OrderPrice, orderDescription.Quantity, orderDescription.AllowPartial);
                 EventHandler<DealExecutedEventArgs> handler = (executedOrder, args) =>
                 {
                     if (order == executedOrder)
                     {
                         // we have been executed
                         investorInstruction.NotifyOrderExecution(args.Quantity, args.Price);
-                        remainingQuantityToBeExecuted -= args.Quantity;
+                        //remainingQuantityToBeExecuted -= args.Quantity;
                     }
                 };
                 market.OrderExecuted += handler;
@@ -72,9 +70,76 @@ namespace SimpleOrderRouting.Journey1
             }
         }
 
+        /// <summary>
+        /// Build the description of the orders needed to fulfill the <see cref="investorInstruction"/>
+        /// </summary>
+        /// <param name="investorInstruction"></param>
+        /// <returns>Order description</returns>
+        private IList<OrderDescription> SolveMarketSweep(InvestorInstruction investorInstruction)
+        {
+            var description = new List<OrderDescription>();
+            // Checks liquidities available to weighted average for execution
+            int remainingQuantityToBeExecuted = investorInstruction.Quantity;
+
+            var requestedPrice = investorInstruction.Price;
+            
+            var availableQuantityOnMarkets = this.ComputeAvailableQuantityForThisPrice(requestedPrice);
+
+            var ratio = (remainingQuantityToBeExecuted / (decimal)availableQuantityOnMarkets);
+
+            foreach (var market in this.markets)
+            {
+                var convertedMarketQuantity = Math.Round((market.SellQuantity * ratio), 2, MidpointRounding.AwayFromZero);
+                var quantityToExecute = Convert.ToInt32(convertedMarketQuantity);
+
+                if (quantityToExecute > 0)
+                {
+                    description.Add(new OrderDescription(market, investorInstruction.Way, quantityToExecute, requestedPrice, true));
+                }
+            }
+
+            return description;
+        }
+
+        private int ComputeAvailableQuantityForThisPrice(decimal requestedPrice)
+        {
+            var availableQuantityOnMarkets = 0;
+
+            foreach (var market in this.markets)
+            {
+                if (requestedPrice >= market.SellPrice)
+                {
+                    availableQuantityOnMarkets += market.SellQuantity;
+                }
+            }
+            return availableQuantityOnMarkets;
+        }
+
         public InvestorInstruction CreateInvestorInstruction(Way way, int quantity, decimal price)
         {
             return new InvestorInstruction(way, quantity, price);
+        }
+
+        private struct OrderDescription
+        {
+            public Market TargetMarket;
+
+            public Way OrderWay;
+
+            public int Quantity;
+
+            public decimal OrderPrice;
+
+            public bool AllowPartial;
+
+            public OrderDescription(Market targetMarket, Way orderWay, int quantity, decimal orderPrice, bool allowPartial)
+            {
+                this.TargetMarket = targetMarket;
+                this.OrderWay = orderWay;
+                this.Quantity = quantity;
+                this.OrderPrice = orderPrice;
+                this.AllowPartial = allowPartial;
+            }
         }
     }
 }

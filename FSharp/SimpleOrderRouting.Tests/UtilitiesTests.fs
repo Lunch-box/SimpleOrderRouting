a4dc8637e5c3518578f8d06b8dc0fb16f469c99d
tjaskula <thomasv1000@hotmail.fr>
2014-11-07 20:09:40 +0100

    Add Utilities tests (with no tests yet) and a reference to main project

---
+++ FSharp/SimpleOrderRouting.Tests/UtilitiesTests.fs
@@ -0,0 +1,12 @@
+﻿namespace SimpleOrderRouting.Tests
+
+open Xunit
+open FsUnit.Xunit
+open SimpleOrderRouting.Journey1.Rop
+
+module ``Rop tests`` =
+
+    [<Fact>]
+    let ``Create a Success with no messages``() =
+        
+        true |> should be True

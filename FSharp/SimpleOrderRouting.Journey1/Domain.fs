405676e12ef207cb54eadb5be4fb093a51a5261c
tjaskula <thomasv1000@hotmail.fr>
2014-11-07 19:28:16 +0100

    Add InvestorInstruction

--- FSharp/SimpleOrderRouting.Journey1/Domain.fs
+++ FSharp/SimpleOrderRouting.Journey1/Domain.fs
@@ -1,6 +1,12 @@
 ﻿namespace SimpleOrderRouting.Journey1
 
 module Domain =
+    
+    open System
+
+    type Way =
+        | Buy
+        | Sell
 
     type Market = {SellQuantity : int; SellPrice : decimal; TimeSent : int}
-    type InvestorInstruction = {Id : single}
+    type InvestorInstruction = {Way : Way; Quantity : int; Price : decimal; AllowPartialExecution : bool; GoodTill : DateTime option}

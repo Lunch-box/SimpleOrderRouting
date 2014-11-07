35989c58021f8ec212a600c83b68924415a30d83
tjaskula <thomasv1000@hotmail.fr>
2014-11-07 20:25:54 +0100

    Modify success unit test. Add Failure branch unit test.

--- FSharp/SimpleOrderRouting.Tests/UtilitiesTests.fs
+++ FSharp/SimpleOrderRouting.Tests/UtilitiesTests.fs
@@ -6,9 +6,19 @@ open SimpleOrderRouting.Journey1.Rop
 
 module ``Rop tests`` =
 
+    let failBranch() =
+        failwith "This branch should not have been called."
+
     [<Fact>]
     let ``Create a Success with no messages``() =
         let result = succeed 10
         match result with
             | Success v -> v |> should equal 10
-            | Failure errors -> errors.Length |> should equal 0
+            | Failure _ -> failBranch()
+
+    [<Fact>]
+    let ``create a Failure with a message``() =
+        let result = fail "This is a failure"
+        match result with
+            | Success _ -> failBranch()
+            | Failure errors -> errors.[0] |> should equal "This is a failure"

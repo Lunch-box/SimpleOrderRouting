e85d28a407725c0c13dc757dc97c460cdeb51294
tjaskula <thomasv1000@hotmail.fr>
2014-11-09 16:33:31 +0100

    Add test for applyR function in utilities

--- FSharp/SimpleOrderRouting.Tests/UtilitiesTests.fs
+++ FSharp/SimpleOrderRouting.Tests/UtilitiesTests.fs
@@ -17,8 +17,18 @@ module ``Rop tests`` =
             | Failure _ -> failBranch()
 
     [<Fact>]
-    let ``create a Failure with a message``() =
+    let ``Create a Failure with a message``() =
         let result = fail "This is a failure"
         match result with
             | Success _ -> failBranch()
             | Failure errors -> errors.[0] |> should equal "This is a failure"
+
+    [<Fact>]
+    let ``Apply the function to the value only if both are Success``() =
+        let success = succeed 10
+        let intToString i = i.ToString()
+        let successFunc = succeed intToString
+        let result = applyR successFunc success
+        match result with
+            | Success r -> r |> should equal "10"
+            | Failure _ -> failBranch()

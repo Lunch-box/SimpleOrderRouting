2231ed6758e2d3293f3b576814fc855fd37fd9f4
tjaskula <thomasv1000@hotmail.fr>
2014-11-09 16:45:20 +0100

    Add complementary unit tests for applyR function from Utilities (failure cases)

--- FSharp/SimpleOrderRouting.Tests/UtilitiesTests.fs
+++ FSharp/SimpleOrderRouting.Tests/UtilitiesTests.fs
@@ -32,3 +32,35 @@ module ``Rop tests`` =
         match result with
             | Success r -> r |> should equal "10"
             | Failure _ -> failBranch()
+
+    [<Fact>]
+    let ``Apply returns Failure if value is not a Success``() =
+        let failure = fail "First error"
+        let intToString i = i.ToString()
+        let successFunc = succeed intToString
+        let result = applyR successFunc failure
+        match result with
+            | Success _ -> failBranch()
+            | Failure errors -> errors.[0] |> should equal "First error"
+
+    [<Fact>]
+    let ``Apply returns Failure if function is not a Success``() =
+        let success = succeed 10
+        let intToString i = i.ToString()
+        let failFunc = fail "Function in error"
+        let result = applyR failFunc success
+        match result with
+            | Success _ -> failBranch()
+            | Failure errors -> errors.[0] |> should equal "Function in error"
+
+    [<Fact>]
+    let ``Apply returns Failure if function and value are not a Success``() =
+        let expectedErrors = ["First error"; "Function in error"]
+        let failure = fail expectedErrors.[0]
+        let intToString i = i.ToString()
+        let failFunc = fail expectedErrors.[1]
+        let result = applyR failFunc failure
+        match result with
+            | Success _ -> failBranch()
+            | Failure errors -> errors |> List.rev
+                                       |> List.iteri (fun i e -> e |> should equal expectedErrors.[i])
